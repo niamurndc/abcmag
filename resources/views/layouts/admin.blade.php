5c953982fe55fa51d7659f024c5a407f9c58a38e
niamurndc <niamurndc17@gmail.com>
2020-08-11 12:07:57 +0600

    project complete

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,8 +12,7 @@
   <title>ABCMagazine | Admin</title>
 
   <!-- Custom fonts for this template-->
-  <link href="/css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="/css/fontawesome.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
